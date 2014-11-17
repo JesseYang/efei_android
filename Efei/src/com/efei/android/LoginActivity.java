1db7777989531eaa99f69fe5d5a6e45b7202bb9e
云中天静 <642209019@qq.com>
2014-11-17 22:15:21 +0800

    remove useless code

--- Efei/src/com/efei/android/LoginActivity.java
+++ Efei/src/com/efei/android/LoginActivity.java
@@ -1,19 +1,11 @@
 package com.efei.android;
 
-import com.efei.lib.android.async.IJob;
-import com.efei.lib.android.async.IUICallback;
-import com.efei.lib.android.bean.net.ReqLogin;
-import com.efei.lib.android.bean.net.RespLogin;
-import com.efei.lib.android.engine.impl.LoginServiceImpl;
-
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,19 +13,17 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.efei.lib.android.async.IJob;
+import com.efei.lib.android.async.IUICallback;
+import com.efei.lib.android.bean.net.ReqLogin;
+import com.efei.lib.android.bean.net.RespLogin;
+import com.efei.lib.android.engine.impl.LoginServiceImpl;
+
 /**
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends Activity
 {
-
-	/**
-	 * A dummy authentication store containing known user names and
-	 * passwords. TODO: remove after connecting to a real authentication
-	 * system.
-	 */
-	private static final String[] DUMMY_CREDENTIALS = new String[]
-	{ "foo@example.com:hello", "bar@example.com:world" };
 	/**
 	 * Keep track of the login task to ensure we can cancel it if requested.
 	 */
